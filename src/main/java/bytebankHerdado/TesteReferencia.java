ccbf0af7782e0a9698ff43bd873aa234d92b8713
kelwyn <krawcykk1@gmail.com>
2025-05-07 22:35:44 -0300

    Adicionando Conta corrente e conta poupanca definindo construtor super

--- src/main/java/bytebankHerdado/TesteReferencia.java
+++ src/main/java/bytebankHerdado/TesteReferencia.java
@@ -18,9 +18,15 @@ public class TesteReferencia {
         Funcionario ev = new EditorVideo();
         ev.setSalario(2500);
 
+        Designer d = new Designer();
+        d.setSalario(2000);
+
+        
+
         ControleBonificacao controle = new ControleBonificacao();
         controle.registra(g1);
         controle.registra(f);
+        controle.registra(ev);
         controle.registra(ev);
 
         System.out.println(controle.getSoma());
